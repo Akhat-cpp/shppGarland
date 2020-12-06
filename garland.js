d71af0b47199efada5665b8bf44ce2d44bd94e0b
Akhat-cpp <56387863+Akhat-cpp@users.noreply.github.com>
2020-12-06 20:39:11 +0100

    Add files via upload

--- garland.js
+++ garland.js
@@ -7,33 +7,35 @@ let width = 1536;
 let height = 788;
 
 let timeout = 150;
-let all = false;
 let mode = 0;
 let counter = -1;
-let arraySequence = [0, 1, 2, 3, 4, 5, 6];
-let arrayColors = ['rgba(0, 0, 255, 1)', 'rgba(255, 0, 0, 1)', 'rgba(255, 255, 0, 1)', 'rgba(0, 255, 0, 1)', 'rgba(0, 255, 255, 1)', 'rgba(255, 0, 255, 1'];
 let color = 'rgba(255, 255, 255, 1)';
 
 img.onload = function()
 {
-	blinkAll(arraySequence, color, timeout, counter, mode);
+	blinkAll(color, timeout, counter, mode);
 }
 
-function blinkAll(arraySequence, color, timeout, counter, mode)//after change mode counter == -1
+function blinkAll(color, timeout, counter, mode, )//after change mode counter == -1
 {
 	ctx.rect(0, 0, 1536, 788);
     ctx.drawImage(img, 0, 0, width, height);
 
+    blink('rgba(255, 255, 255, 1)', 193, 137, 7.5, 0, 2 * Math.PI, true, 55);
+    blink('rgba(255, 255, 255, 1)', 1306, 127, 7.5, 0, 2 * Math.PI, true, 55);
+    blink('rgba(255, 255, 255, 1)', 753, 650.5, 7.5, 0, 2 * Math.PI, true, 55);
+    blink('rgba(255, 255, 255, 1)', 1243, 531.5, 7.5, 0, 2 * Math.PI, true, 55);
+
     if(mode == 0)
     {
-		(variableReturn('counter') == arraySequence.length-1) ? (counterZero()) : (counterPlusPlus());
-		if(arraySequence[variableReturn('counter')] == 0) {blink(color, 203, 474, 37, 0, 2 * Math.PI, true);  setTimeout(() => blinkAll(arraysReturn('arraySequence'), variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
-		if(arraySequence[variableReturn('counter')] == 1) {blink(color, 372, 407.5, 22, 0, 2 * Math.PI, true); setTimeout(() => blinkAll(arraysReturn('arraySequence'), variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
-		if(arraySequence[variableReturn('counter')] == 2) {blink(color, 581.5, 369.5, 37, 0, 2 * Math.PI, true); setTimeout(() => blinkAll(arraysReturn('arraySequence'), variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
-		if(arraySequence[variableReturn('counter')] == 3) {blink(color, 747.5, 273, 22, 0, 2 * Math.PI, true); setTimeout(() => blinkAll(arraysReturn('arraySequence'), variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
-		if(arraySequence[variableReturn('counter')] == 4) {blink(color, 1015, 304, 37, 0, 2 * Math.PI, true); setTimeout(() => blinkAll(arraysReturn('arraySequence'), variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
-		if(arraySequence[variableReturn('counter')] == 5) {blink(color, 1193.5, 333, 22, 0, 2 * Math.PI, true); setTimeout(() => blinkAll(arraysReturn('arraySequence'), variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
-		if(arraySequence[variableReturn('counter')] == 6) {blink(color, 1349, 404, 37, 0, 2 * Math.PI, true); setTimeout(() => blinkAll(arraysReturn('arraySequence'), variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
+		(variableReturn('counter') == 6) ? (counterZero()) : (counterPlusPlus());
+		if(variableReturn('counter') == 0) {blink(color, 203, 474, 37, 0, 2 * Math.PI, true);  setTimeout(() => blinkAll(variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
+		if(variableReturn('counter') == 1) {blink(color, 372, 407.5, 22, 0, 2 * Math.PI, true); setTimeout(() => blinkAll(variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
+		if(variableReturn('counter') == 2) {blink(color, 581.5, 369.5, 37, 0, 2 * Math.PI, true); setTimeout(() => blinkAll(variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
+		if(variableReturn('counter') == 3) {blink(color, 747.5, 273, 22, 0, 2 * Math.PI, true); setTimeout(() => blinkAll(variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
+		if(variableReturn('counter') == 4) {blink(color, 1015, 304, 37, 0, 2 * Math.PI, true); setTimeout(() => blinkAll(variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
+		if(variableReturn('counter') == 5) {blink(color, 1193.5, 333, 22, 0, 2 * Math.PI, true); setTimeout(() => blinkAll(variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
+		if(variableReturn('counter') == 6) {blink(color, 1349, 404, 37, 0, 2 * Math.PI, true); setTimeout(() => blinkAll(variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);}
     }
 	if(mode == 1)
 	{	
@@ -44,19 +46,19 @@ function blinkAll(arraySequence, color, timeout, counter, mode)//after change mo
 		blink(color, 1015, 304, 37, 0, 2 * Math.PI, true);
 		blink(color, 1193.5, 333, 22, 0, 2 * Math.PI, true);
 		blink(color, 1349, 404, 37, 0, 2 * Math.PI, true);
-		setTimeout(() => blinkAll(arraysReturn('arraySequence'), variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);
+		setTimeout(() => blinkAll(variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);
 	}
 	if(mode == 2)
 	{
 		(variableReturn('counter') == 1) ? (counterZero()) : (counterPlusPlus());
-		if(counter == 0) {blink(color, 203, 474, 37, 0, 2 * Math.PI, true);blink(color, 203, 474, 37, 0, 2 * Math.PI, true);}
+		if(counter == 0) {blink(color, 203, 474, 37, 0, 2 * Math.PI, true);blink(color, 203, 474, 37, 0, 2 * Math.PI, true);}	
 		if(counter == 1) {blink(color, 372, 407.5, 22, 0, 2 * Math.PI, true);blink(color, 372, 407.5, 22, 0, 2 * Math.PI, true);}
 		if(counter == 0) {blink(color, 581.5, 369.5, 37, 0, 2 * Math.PI, true);blink(color, 581.5, 369.5, 37, 0, 2 * Math.PI, true);}
 		if(counter == 1) {blink(color, 747.5, 273, 22, 0, 2 * Math.PI, true);blink(color, 747.5, 273, 22, 0, 2 * Math.PI, true);}
 		if(counter == 0) {blink(color, 1015, 304, 37, 0, 2 * Math.PI, true);blink(color, 1015, 304, 37, 0, 2 * Math.PI, true);}
 		if(counter == 1) {blink(color, 1193.5, 333, 22, 0, 2 * Math.PI, true);blink(color, 1193.5, 333, 22, 0, 2 * Math.PI, true);}
 		if(counter == 0) {blink(color, 1349, 404, 37, 0, 2 * Math.PI, true);blink(color, 1349, 404, 37, 0, 2 * Math.PI, true);}
-		setTimeout(() => blinkAll(arraysReturn('arraySequence'), variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);
+		setTimeout(() => blinkAll(variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);
 	}
 	if(mode == 3)
 	{
@@ -68,7 +70,7 @@ function blinkAll(arraySequence, color, timeout, counter, mode)//after change mo
 		if(counter == 0) {blink('rgba(0, 0, 255, 1)', 1015, 304, 37, 0, 2 * Math.PI, true); blink('rgba(0, 0, 255, 1)', 1015, 304, 37, 0, 2 * Math.PI, true);}
 		if(counter == 1) {blink('rgba(0, 255, 0, 1)', 1193.5, 333, 22, 0, 2 * Math.PI, true); blink('rgba(0, 255, 0, 1)', 1193.5, 333, 22, 0, 2 * Math.PI, true);}
 		if(counter == 0) {blink('rgba(0, 0, 255, 1)', 1349, 404, 37, 0, 2 * Math.PI, true); blink('rgba(0, 0, 255, 1)', 1349, 404, 37, 0, 2 * Math.PI, true);}
-		setTimeout(() => blinkAll(arraysReturn('arraySequence'), variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);
+		setTimeout(() => blinkAll(variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);
 	}
 	if(mode == 4)
 	{
@@ -80,7 +82,7 @@ function blinkAll(arraySequence, color, timeout, counter, mode)//after change mo
 		if(counter == 0) {blink('rgba(0, 255, 0, 1)', 1015, 304, 37, 0, 2 * Math.PI, true, 85); blink('rgba(0, 255, 0, 1)', 1015, 304, 37, 0, 2 * Math.PI, true, 45);}
 		if(counter == 1) {blink('rgba(255, 0, 0, 1)', 1193.5, 333, 22, 0, 2 * Math.PI, true, 85); blink('rgba(255, 0, 0, 1)', 1193.5, 333, 22, 0, 2 * Math.PI, true, 45);}
 		if(counter == 0) {blink('rgba(0, 0, 255, 1)', 1349, 404, 37, 0, 2 * Math.PI, true, 85); blink('rgba(0, 0, 255, 1)', 1349, 404, 37, 0, 2 * Math.PI, true, 45);}
-		setTimeout(() => blinkAll(arraysReturn('arraySequence'), variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);
+		setTimeout(() => blinkAll(variableReturn('color'), variableReturn('timeout'), variableReturn('counter'), variableReturn('mode')), timeout);
 	}
 }
 
@@ -105,6 +107,7 @@ addEventListener("keydown", function(e)
 	if(e.key == 'ArrowDown') speed(true); 
 	if(e.key == 'ArrowRight') nextMode(true);
 	if(e.key == 'ArrowLeft') nextMode(false);
+	if(e.key == ' ') modal();
 });
 
 function speed(upOrDown)
@@ -145,7 +148,9 @@ function variableReturn(variable)
 	return eval(variable);
 }
 
-function arraysReturn(array)
+//modal 1 time
+function modal()
 {
-	return eval(array);
+	document.getElementById('modal1').classList.add('unvisivle');
+	document.getElementById('canvas').classList.remove('unvisible');
 }
